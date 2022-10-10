4f9f21b4afc5380be2bf0dd17b50bd0678716690
DIWAKAR SHARMA <diwakarsharma2216@gmail.com>
2022-10-10 20:02:44 +0530

    variable

---
+++ W23-variable/problem2.js
@@ -0,0 +1,8 @@
+let name="Diwakar Sharma";
+console.log(name);
+
+ name="Umesh Sharma";
+console.log(name);
+
+name="Vineeta Sharma";
+console.log(name)
---
+++ W23-variable/problem4.js
@@ -0,0 +1,16 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+
+let name="Diwakar Sharma";
+let school="Radhika senior secondary";
+let grade=12;
+let math_marks=78;
+let eng_mark=89;
+let sci_mark=90;
+
+console.log("********Report-Card********");
+console.log("Name =>",name);
+console.log("school =>",school);
+console.log("grade=>",grade);
+console.log("mark_math=>",math_marks);
+console.log("marks_eng",eng_mark)
+console.log("marks_sci",sci_mark)
